1382618e5b1cce2713c9cc89ce32c4d096770bd9
Mark May <mamark90@gmail.com>
2020-09-01 21:06:45 +0000

    Automatically backed up by Learn

---
+++ app/models/hero.rb
@@ -0,0 +1,16 @@
+class Hero 
+  attr_reader :name, :motto
+  
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @motto = params[:motto]
+    @@all << self
+  end   
+
+  def self.all
+    @@all
+  end
+
+end
